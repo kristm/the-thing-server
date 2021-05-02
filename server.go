0018513b3b263d83ae058bd8e4033d30fb403e87
Krist Menina <krist@hhww.org>
2021-05-02 21:46:22 +0800

    🤘

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import "fmt"
 import "encoding/json"
 import "io/ioutil"
 import "time"
+import "regexp"
 import "strconv"
 import "github.com/gofiber/fiber/v2"
 import "github.com/gofiber/fiber/v2/middleware/cache"
@@ -61,8 +62,7 @@ func authQs(config *Config) url.Values {
 	return q
 }
 
-func apiRequest(url string) ([]byte, error) {
-	//marvelUrl := "https://gateway.marvel.com:443/v1/public/characters"
+func fetchCharacters(url string, offset int) ([]byte, error) {
 	client := &http.Client{}
 	request, err := http.NewRequest("GET", url, nil)
 
@@ -72,12 +72,16 @@ func apiRequest(url string) ([]byte, error) {
 	}
 
 	qs := authQs(&config)
-	qs.Add("limit", "100")
-	qs.Add("offset", "100")
+
+	characterQuery, _ := regexp.MatchString("[0-9]$", url)
+	if !characterQuery {
+		qs.Add("limit", "100")
+		qs.Add("offset", strconv.Itoa(offset))
+	}
+
 	request.URL.RawQuery = qs.Encode()
 	request.Header.Set("content-type", "application/json; charset=UTF-8")
 
-	// 1493 characters as of may 2
 	response, err := client.Do(request)
 
 	defer response.Body.Close()
@@ -88,6 +92,7 @@ func apiRequest(url string) ([]byte, error) {
 	}
 
 	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Printf("%s: %s\n", response.Status, request.URL.Path)
 	return body, nil
 }
 
@@ -96,7 +101,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cache.New(cache.Config{
-		Expiration:   10 * time.Minute,
+		Expiration:   1 * time.Hour,
 		CacheControl: true,
 	}))
 
@@ -105,64 +110,37 @@ func main() {
 	})
 
 	app.Get("/characters", func(c *fiber.Ctx) error {
-		body, err := apiRequest("https://gateway.marvel.com:443/v1/public/characters")
-
-		if err != nil {
-			return c.SendString(err.Error())
-		}
-
 		var data ApiResponse
-		json.Unmarshal(body, &data)
-		sm := Character{
-			Id:          -1,
-			Name:        "Strange Mouse",
-			Description: "Lab Rat",
-		}
-
-		data.Data.Results = append(data.Data.Results, sm)
-
-		fmt.Printf("characters: %v", data.Data.Results)
-
-		for _, character := range data.Data.Results {
-			fmt.Println(">> ", character.Id, character.Name)
-		}
-
 		charIds := []int{}
-		for _, char := range data.Data.Results {
-			charIds = append(charIds, char.Id)
+
+		// 1493 characters as of may 2
+		for i := 0; i <= 15; i++ {
+			body, err := fetchCharacters("https://gateway.marvel.com:443/v1/public/characters", i*100)
+			if err != nil {
+				return c.SendString(err.Error())
+			}
+			json.Unmarshal(body, &data)
+			for _, char := range data.Data.Results {
+				charIds = append(charIds, char.Id)
+			}
 		}
+
 		out, _ := json.Marshal(charIds)
 		return c.SendString(string(out))
 	})
 
 	app.Get("/characters/:id", func(c *fiber.Ctx) error {
-		marvelUrl := fmt.Sprintf("%s%s", "https://gateway.marvel.com:443/v1/public/characters/", c.Params("id"))
-		client := &http.Client{}
-		request, err := http.NewRequest("GET", marvelUrl, nil)
-
-		if err != nil {
-			fmt.Printf("Request Error %v", err)
-		}
-
-		request.URL.RawQuery = authQs(&config).Encode()
-
-		request.Header.Set("content-type", "application/json; charset=UTF-8")
-
-		response, err := client.Do(request)
-
-		defer response.Body.Close()
-
-		if err != nil {
-			fmt.Printf("Response Error %v", err)
-		}
-
 		var char ApiResponse
-		body, _ := ioutil.ReadAll(response.Body)
+		body, err := fetchCharacters(fmt.Sprintf("%s%s", "https://gateway.marvel.com:443/v1/public/characters/", c.Params("id")), 0)
+
+		if err != nil {
+			return c.SendString(err.Error())
+		}
+
 		json.Unmarshal(body, &char)
 
 		out, _ := json.Marshal(char.Data.Results)
 		return c.SendString(string(out))
-
 	})
 
 	app.Listen(":8080")
